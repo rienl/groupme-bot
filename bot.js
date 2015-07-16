4e95cdea5955829881eba9fccdb3d9793e8f8a85
Kyle Wilcox <wilcox.kyle@gmail.com>
2015-07-16 12:35:00 -0400

    Keep a group to bot mapping to send message to the same room

--- bot.js
+++ bot.js
@@ -7,7 +7,6 @@ var motdIDs = process.env.MOTD_IDS.split(",");
 var motdUrl = process.env.MOTD_URL;
 
 var pageIDS = process.env.PAGE_IDS.split(",");
-var botIDS = process.env.BOT_IDS.split(",");
 
 var botName = "H.P. Lovecraft"
 
@@ -16,6 +15,15 @@ var postOptions = {
                     path: '/v3/bots/post',
                     method: 'POST'
                   };
+
+
+var group_bot_map = {}
+
+var mapping = process.env.GROUP_BOT_MAP.split(",")
+for (x in mapping) {
+    var s = mapping[x].split(';');
+    group_bot_map[s[0]] = s[1];
+}
 
 
 function respond() {
@@ -49,12 +57,19 @@ function getMotd(callback) {
 }
 
 
+function bot_for_group(request) {
+    if (request.group_id in group_bot_map) {
+        return group_bot_map[request.group_id];
+    } else {
+        return null;
+    }
+}
+
+
 function postMotd() {
     getMotd(function(message) {
-        if (message != null) {
-            for (i in motdIDs) {
-                postMessage(motdIDs[i], message);
-            }
+        for (i in motdIDs) {
+            postMessage(motdIDs[i], message);
         }
     });
     this.res.writeHead(200);
@@ -64,7 +79,7 @@ function postMotd() {
 
 function page(request) {
     if (request.group_id == moderatorsGroup) {
-        if (request.text.indexOf("!page") > -1) {
+        if (request.text.indexOf("!page") === 0) {
             message = request.text.replace("!page", "");
             for (i in pageIDS) {
                 postMessage(pageIDS[i], message);
@@ -75,40 +90,46 @@ function page(request) {
 
 
 function motd(request) {
-    if (request.text == "!motd") {
+    if (request.text.indexOf("!motd") === 0) {
         getMotd(function(message) {
-            if (message != null) {
-                postMessage(motdIDs[i], message);
-            }
+            postMessage(bot_for_group(request), message);
         });
     }
 }
 
 
 function postMessage(botID, message) {
-  var body, botReq;
+    if (message == null) {
+        console.log("Skipping sending of null message")
+        return
+    }
+    if (botID == null) {
+        console.log("Skipping sending of null botID")
+        return
+    }
 
-  body = {
-    "bot_id" : botID,
-    "text" : message
-  };
+    var body, botReq;
+    body = {
+        "bot_id" : botID,
+        "text" : message
+    };
 
-  console.log('--------\n' + message + '\n\nSent as ' + botID + '\n--------');
+    console.log('--------\n' + message + '\n\nSent as ' + botID + '\n--------');
 
-  botReq = HTTPS.request(postOptions, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
+    botReq = HTTPS.request(postOptions, function(res) {
+        if(res.statusCode == 202) {
+            //neat
+        } else {
+            console.log('rejecting bad status code ' + res.statusCode);
+        }
+    });
+    botReq.on('error', function(err) {
+        console.log('error posting message '  + JSON.stringify(err));
+    });
+    botReq.on('timeout', function(err) {
+        console.log('timeout posting message '  + JSON.stringify(err));
+    });
+    botReq.end(JSON.stringify(body));
 }
 
 exports.respond = respond;
